af925a129f781868feb2eabc8142f9cb31da529c
Trotter Cashion <cashion@gmail.com>
2013-03-14 09:54:13 -0700

    initial commit

---
+++ src/main/java/com/trottercashion/karyon-test/App.java
@@ -0,0 +1,13 @@
+package com.trottercashion.karyon-test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
